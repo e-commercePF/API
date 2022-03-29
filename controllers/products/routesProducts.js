91e56f8c84225a64b4aead707b5b5c4a2e3bf391
ocampojuanmartin <ocampojuanmartin40@gmail.com>
2022-03-29 01:37:39 -0300

    merged filters in one route

--- controllers/products/routesProducts.js
+++ controllers/products/routesProducts.js
@@ -1,14 +1,16 @@
 const productRouter = require('express').Router();
 const Product = require('../../models/products/Product');
 const Category = require('../../models/products/Category')
-const { getProducts,getProductByName, filterRange, getProductsById, updateProduct, filterByBrand, createProduct, deleteProduct, sortPriceDesc, 
-    sortPriceAsc, sortNameAsc, sortNameDesc, filterByCategory, getBrands, getProductsforpage} = require('../../controllers/products/productFunctions')
+const { createProduct, getProducts, getProductByName, getBrands, getProductsById, updateProduct, deleteProduct, getPaginatedFilters, filterRange} = require('../../controllers/products/productFunctions')
+
+
+// POST || http://localhost:3000/api/products/create
+productRouter.post('/create', createProduct)
 
 // GET || http://localhost:3000/api/products
 productRouter.get('/',getProducts)
-
-// GET || http://localhost:3000/api/products/forPage
-productRouter.get('/forPage', getProductsforpage)
+// GET || http://localhost:3000/api/products/brands
+productRouter.get('/brands', getBrands)
 
 // GET || http://localhost:3000/api/products/:name
 productRouter.get('/name/:name', getProductByName)
@@ -16,38 +18,19 @@ productRouter.get('/name/:name', getProductByName)
 // GET || http://localhost:3000/api/products/:id
 productRouter.get('/id/:id', getProductsById)
 
-// POST || http://localhost:3000/api/products/create
-productRouter.post('/create', createProduct)
+// PUT || http://localhost:3000/api/products/update/:id
+productRouter.put('/update/:id', updateProduct)
 
 // DELETE || http://localhost:3000/api/products/delete/:id
 productRouter.delete('/delete/:id', deleteProduct)
 
-// PUT || http://localhost:3000/api/products/update/:id
-productRouter.put('/update/:id', updateProduct)
-
-// GET || http://localhost:3000/api/products/desc
-productRouter.get('/desc', sortPriceDesc)
-
-// GET || http://localhost:3000/api/products/asc
-productRouter.get('/asc', sortPriceAsc)
-
-// GET || http://localhost:3000/api/products/nameA
-productRouter.get('/nameA', sortNameAsc)
-
-// GET || http://localhost:3000/api/products/nameZ
-productRouter.get('/nameZ', sortNameDesc)
-
-// GET || http://localhost:3000/api/products/category
-productRouter.get('/category', filterByCategory)
-
-// GET || http://localhost:3000/api/products/brand
-productRouter.get('/brand', filterByBrand)
-
 // GET || http://localhost:3000/api/products/range
 productRouter.get('/range', filterRange)
 
-// GET || http://localhost:3000/api/products/brands
-productRouter.get('/brands', getBrands)
+// GET || http://localhost:3000/api/products/forPage
+productRouter.get('/forPage', getPaginatedFilters)
+
+
 
 
 
