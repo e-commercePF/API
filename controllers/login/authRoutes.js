340958e1867b7a00a216c4f6824e877158cfd526
nicodssss <ngdiaz98@gmail.com>
2022-04-02 15:43:10 -0300

    update more verifications in signup and forcepasswordreset

--- controllers/login/authRoutes.js
+++ controllers/login/authRoutes.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
-const { signup, signin, googlelogin, activateAccount, forgotPassword, resetPassword} = require('./authFunctions');
+const { signup, signin, googlelogin, activateAccount, forgotPassword, resetPassword, forceResetPassword} = require('./authFunctions');
+const { authenticateJWT } = require('../login/authFunctions');
 
 router.post('/signup', signup);
 router.post('/signin', signin);
@@ -9,6 +10,8 @@ router.post('/googlelogin', googlelogin)
 
 router.put('/forgot-password', forgotPassword)
 router.put('/reset-password', resetPassword)
+// GET || http://localhost:3000/api/auth/force-reset-password --> por body { _id: x._id } y por header / config ={ headers: {Authorization: "Bearer" + token} }
+router.put('/force-reset-password', authenticateJWT, forceResetPassword)
 
 
 
