2aa5fdac28c9f085dc594367414a90a45f295f9f
nicodssss <ngdiaz98@gmail.com>
2022-04-05 23:13:16 -0300

    update db.js

--- db.js
+++ db.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
+const urlDb = process.env.MONGO_DB_URL
 
-
-mongoose.connect(process.env.MONGO_DB_URL)
+mongoose.connect(urlDb, { useNewUrlParser: true })
     .then(database => {
         console.log('Database is connected')
     })
